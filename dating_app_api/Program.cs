4593f6eb1fb5bef9653540ef743444512c25364e
Piolo Fano <efano@you-source.com>
2022-07-18 13:59:54 +0800

    End of Section 3 Udemy Dating App

--- dating_app_api/Program.cs
+++ dating_app_api/Program.cs
@@ -12,11 +12,18 @@ builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServerConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
+
+app.UseCors(policy =>
+{
+    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+});
 
 app.UseAuthorization();
 
